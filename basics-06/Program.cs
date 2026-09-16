637c12eaa203fcffbab68e6dcf8c08f023929e62
ali kamal <ali123kamal12@gmail.com>
2026-09-16 13:44:54 +0300

    2nd answer

--- basics-06/Program.cs
+++ basics-06/Program.cs
@@ -6,8 +6,17 @@
         {
             #region 1st answer
 
-            double[] array = { 25.5, 40.0, 33.75 };
-            Console.WriteLine(array[1]);
+            //double[] array = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(array[1]);
+            #endregion
+
+            #region second answer
+            int[,] shelfCopies =
+            {
+                {3,5},
+                {1,4}
+            };
+            Console.WriteLine(shelfCopies[1,0]);
             #endregion
         }
     }
